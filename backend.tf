f610df1d41de0b7aedf80b88b347a9fc47ab5edb
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-15 11:37:32 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform{
   backend "s3" {
-    bucket         = "open-projecttask123 "
-    key            = "terraform/openproject"
+    bucket         = "open-projecttask123"
+    key            = "terraform/state"
     region         = "us-west-2"
     encrypt        = true
   }
