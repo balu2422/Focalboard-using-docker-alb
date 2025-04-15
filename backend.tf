319c3ca128e98154e3de06620df4631b362dde3a
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-15 10:08:28 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "open-projecttask123"  # Replace with your unique S3 bucket name
-    region = "us-east-1"  # Replace with your desired AWS region
+    region = "us-west-2"  # Replace with your desired AWS region
 
   }
 }
