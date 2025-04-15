fb7c26bdd98da59bcd626ceef2d4f5871066fcb0
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-15 10:30:17 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "open-projecttask123"  # Replace with your unique S3 bucket name
+    key    = "path/to/terraform.tfstate"  # Specify the path to the state file inside the bucket
     region = "us-west-2"  # Replace with your desired AWS region
   }
 }
