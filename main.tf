25f94e5188499bfdf41a6972a63e9c8bb8be8c9d
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-15 10:58:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,34 +4,51 @@ provider "aws" {
 
 module "vpc" {
   source = "./modules/vpc"
-  cidr_block = var.vpc_cidr_block
+  cidr_block = var.cidr_block
+  public_subnet1_cidr = var.public_subnet1_cidr
+  public_subnet2_cidr = var.public_subnet2_cidr
+  availability_zone1 = var.availability_zone1
+  availability_zone2 = var.availability_zone2
 }
 
 module "security_groups" {
-  source = "./modules/sg"
+  source = "./modules/security_groups"
   vpc_id = module.vpc.vpc_id
 }
 
 module "ec2" {
   source = "./modules/ec2"
-  ami = var.ec2_ami
-  instance_type = var.ec2_instance_type
-  subnet_id = module.vpc.public_subnet_id
-  security_group_id = module.security_groups.ec2_sg_id  # Corrected argument name
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo amazon-linux-extras install docker -y
-              sudo service docker start
-              sudo usermod -a -G docker ec2-user
-              docker run -d -p 8080:80 -e OPENPROJECT_SECRET_KEY_BASE=secret -e OPENPROJECT_HTTPS=false openproject/openproject:15
-              EOF
+  ami = var.ami
+  instance_type = var.instance_type
+  subnet_id = module.vpc.public_subnet1_id
+  security_group_id = module.security_groups.ec2_security_group_id
+  user_data = var.user_data
 }
 
 module "alb" {
   source = "./modules/alb"
   vpc_id = module.vpc.vpc_id
-  subnet_ids = [module.vpc.public_subnet_id]
-  security_group_id = module.security_groups.alb_sg_id
+  subnet_ids = [module.vpc.public_subnet1_id, module.vpc.public_subnet2_id]
+  security_group_id = module.security_groups.alb_security_group_id
   target_instance_id = module.ec2.instance_id
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet1_id" {
+  value = module.vpc.public_subnet1_id
+}
+
+output "public_subnet2_id" {
+  value = module.vpc.public_subnet2_id
+}
+
+output "ec2_security_group_id" {
+  value = module.security_groups.ec2_security_group_id
+}
+
+output "alb_security_group_id" {
+  value = module.security_groups.alb_security_group_id
 }
