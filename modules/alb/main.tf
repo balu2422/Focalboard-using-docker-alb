82c60de055639b7e1d2452b9b1a4d006a80922d2
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-15 09:31:07 +0530

    Create main.tf

---
+++ modules/alb/main.tf
@@ -0,0 +1,47 @@
+resource "aws_lb" "main" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.security_group_id]
+  subnets            = var.subnet_ids
+
+  tags = {
+    Name = "MyALB"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = "my-targets"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/login"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "MyTargetGroup"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web" {
+  target_group_arn = aws_lb_target_group.main.arn
+  target_id        = aws_instance.web.id
+  port             = 80
+}
