7e7cfd78cdd739750efe57105c157d0191f7e711
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-15 11:36:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-  required_version = "~> 1.11.0"
-}
-
 provider "aws" {
   region = "us-west-2"
 }
