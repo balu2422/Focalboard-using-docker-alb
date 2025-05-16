31d8d980e13765b76fc6dac69ef9677599dbd30e
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-05-16 11:06:17 +0530

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -7,8 +7,8 @@ resource "aws_security_group" "main" {
   vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 8000
+    to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -35,8 +35,8 @@ resource "aws_security_group" "main" {
    vpc_id = var.vpc_id
 
    ingress {
-     from_port   = 80
-     to_port     = 80
+     from_port   = 8000
+     to_port     = 8000
      protocol    = "tcp"
      cidr_blocks = ["0.0.0.0/0"]
    }
